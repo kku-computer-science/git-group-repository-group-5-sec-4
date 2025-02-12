a3a810f3acc5018cd506e68e3d7b923e242a5d05
Pipatsorn <pipatsorn.m@kkumail.com>
2025-02-13 01:53:56 +0700

    add file for merge

---
+++ config/languages.php
@@ -0,0 +1,18 @@
+
+<?php
+
+return [
+    'en' => [
+        'display' => 'English',
+        'flag-icon' => 'us'
+    ],
+    'th' => [
+        'display' => 'ไทย',
+        'flag-icon' => 'th'
+    ],
+    'cn' => [
+        'display' => 'จีน',
+        'flag-icon' => 'cn'
+    ],
+    
+];
---
+++ resources/lang/th/message.php
@@ -0,0 +1,60 @@
+<?php
+return [
+'Home' => 'หน้าแรก',
+'Researchers' => 'สาขาวิชา',
+'ResearchProj' => 'โครงการวิจัย',
+'ResearchGroup' => 'กลุ่มวิจัย',
+'Report' => 'รายงาน',
+'details' =>'รายละเอียดเพิ่มเติม',
+'expertise' =>'ความเชี่ยวชาญ',
+'publications' => 'ผลงานตีพิมพ์ (5 ปี ย้อนหลัง)',
+'education'=>'การศึกษา',
+'publications2' => 'ผลงานตีพิมพ์',
+    'project_service' => 'โครงการบริการวิชาการ/ โครงการวิจัย',
+    'no' => 'ลำดับ',
+    'year' => 'ปี',
+    'project_name' => 'ชื่อโครงการ',
+    'details' => 'รายละเอียด',
+    'project_responsible' => 'ผู้รับผิดชอบโครงการ',
+    'status' => 'สถานะ',
+    'project_duration' => 'ระยะเวลาโครงการ',
+    'to' => 'ถึง',
+    'research_fund_type' => 'ประเภททุนวิจัย',
+    'funding_agency' => 'หน่วยงานที่สนับสนุนทุน',
+    'responsible_department' => 'หน่วยงานที่รับผิดชอบ',
+    'budget_allocated' => 'งบประมาณที่ได้รับจัดสรร',
+    'baht' => 'บาท',
+    'submitted' => 'ยื่นขอ',
+    'in_progress' => 'ดำเนินการ',
+    'closed' => 'ปิดโครงการ',
+    'Summary'=> 'งานวิจัยทั้งหมด',
+    'ExportToExcel' => 'ส่งออกไฟล์ Excel',
+    'No' => 'ลำดับ',
+    'Year' => 'ปี',
+    'PaperName' => 'ชื่อบทความ',
+    'Author' => 'ผู้แต่ง',
+    'DocumentType' => 'ประเภทเอกสาร',
+    'Page' => 'หน้าที่',
+    'Journals/Transactions' => 'วารสารที่ตีพิมพ์',
+    'Ciations' => 'การอ้างอิง',
+    'Doi' => 'รหัสที่ใช้ระบุเอกสารดิจิตอล',
+    'Source' => 'แหล่งที่มา',
+    'Number' => 'ลำดับ',
+    'Name' => 'ชื่อหนังสือ',
+    'Placeofpublication' => 'สถานที่พิมพ์',
+    'Type' => 'ประเภท',
+    'Registrationnumber' => 'หมายเลขทะเบียน',
+    'Dateofregistration' => 'วันที่จดทะเบียน',
+    'Search' => 'ค้นหา',
+    'Book' => 'หนังสือ',
+    'Otheracademicworks' => 'ผลงานวิชาการด้านอื่นๆ',
+   
+    
+  
+
+
+
+];
+
+
+
